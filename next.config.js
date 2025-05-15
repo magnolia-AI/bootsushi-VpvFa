17868597892d8f1930b38f253a89bbd553923426
magnolia-AI <github@antonmagnusson.se>
2025-05-15 22:03:45 +0200

    Update 1 files

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'credentialless' },
           { key: 'Cross-Origin-Resource-Policy', value: 'cross-origin' }
         ],
       },
